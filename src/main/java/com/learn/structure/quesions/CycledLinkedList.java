203ffee05519566cd6fd524dbd6a02a99b4dfbfd
Spike <renziyang1999@gmail.com>
2023-02-14 12:35:14 +0800

    LetCode question

---
+++ src/main/java/com/learn/structure/quesions/CycledLinkedList.java
@@ -0,0 +1,15 @@
+package com.learn.structure.quesions;
+
+/**
+ * @author : Spike
+ * @date : 2023/2/12
+ *
+ * head = [3,2,0,-4], pos = 1
+ */
+public class CycledLinkedList {
+
+    public boolean hasCycle(ListNode head) {
+        
+        return false;
+    }
+}

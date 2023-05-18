05dacef37ee2aea20debe33bd899a5cbce755af4
Spike <renziyang1999@gmail.com>
2023-05-18 15:32:01 +1200

    LetCode question

---
+++ src/main/java/com/learn/structure/quesions/SmallestInfiniteSet.java
@@ -0,0 +1,32 @@
+package com.learn.structure.quesions;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+class SmallestInfiniteSet {
+    int cur;
+    Set<Integer> s;
+
+
+    public SmallestInfiniteSet() {
+        cur = 1;
+        s = new HashSet<>();
+    }
+    public Integer popSmallest() {
+        if (!s.isEmpty()) {
+            int min = Collections.min(s);
+            s.remove(min);
+            return min;
+        }
+        cur ++;
+        return cur - 1;
+
+    }
+
+    public void addBack(int num) {
+        if (cur > num) {
+            s.add(num);
+        }
+    }
+}

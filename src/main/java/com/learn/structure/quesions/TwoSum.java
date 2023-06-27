877d7741b5c08b81de1fcdd6c5f11dec14d69c7e
Spike <renziyang1999@outlook.com>
2023-06-27 22:03:12 +1200

    two sum question

---
+++ src/main/java/com/learn/structure/quesions/TwoSum.java
@@ -0,0 +1,33 @@
+package com.learn.structure.quesions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> valueMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= target) {
+                if (valueMap.containsKey(target - nums[i])) {
+                    result[1] = i;
+                    result[0] = valueMap.get(target - nums[i]);
+                    break;
+                }
+                valueMap.put(nums[i], i);
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-3,4,3,90};
+        int target = 0;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
